8daaf7fadbd733ed0d3ae32706da489874d10827
Jason Chu <jasonchujc@gmail.com>
2016-04-10 17:45:00 +0800

    Fix more import paths and errors

--- cmd/pdserve.go
+++ cmd/pdserve.go
@@ -9,40 +9,40 @@
 package main
 
 import (
-  "fmt"
-  "http"
-  "io"
-  "os"
-  "pdfread"
-  "strm"
-  "svg"
+	"fmt"
+	"github.com/1lann/go-pdfreader"
+	"github.com/1lann/go-pdfreader/strm"
+	"github.com/1lann/go-pdfreader/svg"
+	"http"
+	"io"
+	"os"
 )
 
 var pd *pdfread.PdfReaderT
 
 // hello world, the web server
 func HelloServer(c *http.Conn, req *http.Request) {
-  c.SetHeader("Content-Type", "image/svg+xml; charset=utf-8")
-  page := strm.Int(req.URL.RawQuery, 1) - 1
-  io.WriteString(c, string(svg.Page(pd, page)))
+	c.SetHeader("Content-Type", "image/svg+xml; charset=utf-8")
+	page := strm.Int(req.URL.RawQuery, 1) - 1
+	io.WriteString(c, string(svg.Page(pd, page)))
 }
 
 func complain(err string) {
-  fmt.Printf("%susage: pdserve foo.pdf\n", err)
-  os.Exit(1)
+	fmt.Printf("%susage: pdserve foo.pdf\n", err)
+	os.Exit(1)
 }
 
 func main() {
-  if len(os.Args) == 1 || len(os.Args) > 2 {
-    complain("")
-  }
-  pd = pdfread.Load(os.Args[1])
-  if pd == nil {
-    complain("Could not load pdf file!\n\n")
-  }
-  http.Handle("/hello", http.HandlerFunc(HelloServer))
-  err := http.ListenAndServe(":12345", nil)
-  if err != nil {
-    panic("ListenAndServe: ", err.String())
-  }
+	if len(os.Args) == 1 || len(os.Args) > 2 {
+		complain("")
+	}
+	pd = pdfread.Load(os.Args[1])
+	if pd == nil {
+		complain("Could not load pdf file!\n\n")
+	}
+	http.Handle("/hello", http.HandlerFunc(HelloServer))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: ", err.String())
+	}
 }
